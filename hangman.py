2dd51814c920d9b18f1b97fc17fb1d72c2e69857
Jorge Lázaro Ruiz <jlazaro@futurespace.es>
2024-03-26 11:00:40 +0100

    store stats

--- hangman.py
+++ hangman.py
@@ -1,6 +1,8 @@
-from pandas import read_csv
+import pandas as pd
 from random import randint
 import os
+import uuid
+from datetime import datetime
 
 
 class Hangman:
@@ -15,7 +17,7 @@ class Hangman:
 
     def load(self, filename: str) -> list[str]:
         """Loads the words in the CSV file and stores them in a list."""
-        df = read_csv(filename, header=None)
+        df = pd.read_csv(filename, header=None)
         words_list = df[0].tolist()
         return words_list
 
@@ -71,7 +73,7 @@ class Hangman:
         """Updates with the letters of the word that the user has guessed correctly."""
         self.word_state = ""
         for letter in self.target_word:
-            self.word_state += letter if letter in self.guessed_letters else "_"
+            self.word_state += letter if letter in self.guessed_letters else "·"
         self.word_state += " "
 
     def is_valid_guess(self, guess: str) -> bool:
@@ -84,10 +86,10 @@ class Hangman:
 
     def game_loop(self) -> bool:
         """Main game loop. Returns True if user guessed the word, False otherwise."""
-        while len(self.failed_letters) < 6 and "_" in self.word_state:
-            os.system("cls")
+        while len(self.failed_letters) < 6 and "·" in self.word_state:
             self.print_game_board()
             guess = input("Guess a letter: ")
+            # os.system("cls")
             if self.is_valid_guess(guess):
                 guess = guess.lower()
                 if guess in self.target_word:
@@ -96,12 +98,96 @@ class Hangman:
                 else:
                     self.failed_letters.append(guess)
                 self.print_game_board()
+            else:
+                print("Invalid guess. Try again.")
 
-        return "_" not in self.word_state
+        return "·" not in self.word_state
 
     def play(self) -> None:
+        """Starts a game of Hangman if there are at least 30 words."""
         if self.get_number_of_words() >= 30:
-            self.target_word = self.select_random_word()
-            self.update_word_state()
-            victory = self.game_loop()
-            print("You won!" if victory else "The word was: '" + self.target_word + "'")
+            username = input("What's your name? ")
+            start_date = datetime.now()
+            game_id = uuid.uuid1().int
+            score = 0
+            round_id = 1
+            while round_id <= 3:
+                self.failed_letters = []
+                self.guessed_letters = []
+                self.target_word = self.select_random_word()
+                self.update_word_state()
+                victory = self.game_loop()
+                score += victory
+                print(
+                    "You won!"
+                    if victory
+                    else "The word was: '" + self.target_word + "'."
+                )
+                self.store_round_stats(
+                    game_id,
+                    self.target_word,
+                    username,
+                    round_id,
+                    len(self.failed_letters),
+                    victory,
+                )
+                round_id += 1
+
+            end_date = datetime.now()
+            self.store_game_stats(game_id, username, start_date, end_date, score)
+
+    def store_round_stats(
+        self, game_id, word, username, round_id, user_tries, victory
+    ) -> None:
+        """Stores the stats in rounds_in_games.csv"""
+        if not os.path.exists("rounds_in_games.csv"):
+            df = pd.DataFrame(
+                columns=[
+                    "game_id",
+                    "word",
+                    "username",
+                    "round_id",
+                    "user_tries",
+                    "victory",
+                ]
+            )
+            df.to_csv("rounds_in_games.csv", index=False)
+        data = {
+            "game_id": game_id,
+            "word": word,
+            "username": username,
+            "round_id": round_id,
+            "user_tries": user_tries,
+            "victory": victory,
+        }
+        df = pd.DataFrame([data])
+        df.to_csv(
+            "rounds_in_games.csv",
+            mode="a",
+            index=False,
+            header=not pd.read_csv("rounds_in_games.csv").empty,
+        )
+
+    def store_game_stats(
+        self, game_id, username, start_date, end_date, final_score
+    ) -> None:
+        """Stores the stats in games.csv"""
+        if not os.path.exists("games.csv"):
+            df = pd.DataFrame(
+                columns=["game_id", "username", "start_date", "end_date", "final_score"]
+            )
+            df.to_csv("games.csv", index=False)
+        data = {
+            "game_id": game_id,
+            "username": username,
+            "start_date": start_date,
+            "end_date": end_date,
+            "final_score": final_score,
+        }
+        df = pd.DataFrame([data])
+        df.to_csv(
+            "games.csv",
+            mode="a",
+            index=False,
+            header=not pd.read_csv("games.csv").empty,
+        )
